3bb43697f41f3a930a4c554bfdd60c7ef993d299
Liyans Mathews <liyansmathews9995@gmail.com>
2024-11-12 11:41:51 +0000

    Add practical8 code

---
+++ practical8/README.md
@@ -0,0 +1,20 @@
+## Practical 8
+This folder contains practical codes.
+
+### Compile Euclid.c and RandomEuclid.c
+
+gcc -o Euclid Euclid.c
+gcc -o RandomEuclid RandomEuclid.c
+
+###Execute
+
+./Euclid<br>
+Please enter two positive integers:<br>
+2 3<br>
+IterativeGCD(2,3)=1<br>
+RecursiveGCD(2,3)=1<br>
+
+./RandomEuclid<br>
+
+IterativeGCD(19,15)=1<br>
+RecursiveGCD(19,15)=1<br>
---
+++ practical8/RandomEuclid.c
@@ -0,0 +1,40 @@
+/* 
+*Iterative and recursive versions of Euclid's algorithm
+* to calculate the gcd of two positive integers.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int iterativeGCD(int a, int b){
+	int temp;
+	while(b!=0){
+		temp = b;
+		b=a%b;
+		a=temp;
+	}
+	return a;
+}
+
+// recursive version of the ame algorithm
+int recursiveGCD(int a, int b){
+	if(b == 0){
+		return a;
+	}
+	else{
+		return(recursiveGCD(b,a%b));
+	}
+}
+
+int main(){
+	int a,b;
+	 // Generate two numbur random positive integers 1 and 20
+	 srand(time(NULL));
+	 a = (rand() % 20) + 1;
+	 b = (rand() % 20) + 1;
+	
+	printf("IterativeGCD(%d,%d)=%d\n",a,b, iterativeGCD(a,b));
+	printf("RecursiveGCD(%d,%d)=%d\n",a,b, recursiveGCD(a,b));
+	return 0;
+}
